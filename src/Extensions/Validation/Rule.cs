18ab79d67227a78db5058d53feb4b3f5d12102cf
Mark Roberts <mmmcaffeine@gmail.com>
2020-10-09 14:50:47 +0100

    Renamed members to avoid name collision

--- src/Extensions/Validation/Rule.cs
+++ src/Extensions/Validation/Rule.cs
@@ -5,32 +5,32 @@ namespace AsIfByMagic.Extensions.Validation
 {
     public class Rule<T> : IRule<T>
     {
-        private Expression<Func<T, bool>> Expression { get; }
-        private Func<T, bool> Predicate { get; }
+        private Expression<Func<T, bool>> PredicateExpression { get; }
+        private Func<T, bool> CompiledPredicate { get; }
 
         public Rule(Expression<Func<T, bool>> expression)
         {
-            Expression = expression;
-            Predicate = Expression.Compile();
+            PredicateExpression = expression;
+            CompiledPredicate = PredicateExpression.Compile();
         }
 
         public Exception CreateException(T value)
         {
-            throw new Exception($"The passed value does not meet the criteria of: '{ Expression }'.");
+            throw new Exception($"The passed value does not meet the criteria of: '{ PredicateExpression }'.");
         }
 
         public bool SatisfiedBy(T value)
         {
-            return Predicate(value);
+            return CompiledPredicate(value);
         }
 
         public static Rule<T> operator &(Rule<T> a, Rule<T> b)
         {
-            var parameterA = a.Expression.Parameters[0];
-            var parameterB = b.Expression.Parameters[0];
+            var parameterA = a.PredicateExpression.Parameters[0];
+            var parameterB = b.PredicateExpression.Parameters[0];
             var visitor = new SubstituteExpressionVisitor(parameterB, parameterA);
-            var body = System.Linq.Expressions.Expression.AndAlso(a.Expression.Body, visitor.Visit(b.Expression.Body));
-            var lambda = System.Linq.Expressions.Expression.Lambda<Func<T, bool>>(body, parameterA);
+            var body = Expression.AndAlso(a.PredicateExpression.Body, visitor.Visit(b.PredicateExpression.Body));
+            var lambda = Expression.Lambda<Func<T, bool>>(body, parameterA);
 
             return new Rule<T>(lambda);
         }
